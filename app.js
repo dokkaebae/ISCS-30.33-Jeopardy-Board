8a6a9b728cc9187b727cd7487b0032e96fa854e5
Kirby Ezekiel <kirbyezekielsantos@gmail.com>
2021-06-01 01:26:24 +0800

    Now storing questions and answers in their own separate arrays with matching indices.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let questions_arr = [];
+let answers_arr = [];
+
+let question_grid = document.querySelectorAll(".question-block");
+let question_row = document.querySelectorAll(".question-row");
+console.log(question_grid);
+
 function get_categories() {
     console.log("getting category...");
     let trivia = fetch("http://jservice.io/api/categories?count=6").then(response => { 
@@ -37,19 +44,69 @@ function push_question(arr_1, arr_2) {
     return arr_2;
 }
 
+function hide_questions() {
+    let val = 0;
+    question_row.forEach(function(row) {
+        if (val === 0) {
+            val += 200;
+        } else {
+            row.childNodes[1].textContent = "$"+val;
+            row.childNodes[3].textContent = "$"+val;
+            row.childNodes[5].textContent = "$"+val;
+            row.childNodes[7].textContent = "$"+val;
+            row.childNodes[9].textContent = "$"+val;
+            row.childNodes[11].textContent = "$"+val;
+            val+=200;
+        }
+    })
+    question_grid.forEach(function(block) {
+        block.classList.replace("question", "hidden");
+    })
+}
+
 function display_questions() {
-    let questions_arr = [];
+    let promise_arr = [];
+
     console.log("displaying categories...")
     Promise.resolve(get_categories()).then(cat => {
        return cat;
     }).then(cat => {
-        return Promise.all(push_question(cat, questions_arr));
+        return Promise.all(push_question(cat, promise_arr));
     }).then(response => {
-        response.forEach(function(res) {
-            console.log("category: " + res.title);
-            console.log(res.clues);
-        })
+        for(let i = 0; i < 6; i++) {
+            question_grid[0+i].textContent = response[i].title;
+            question_grid[6+i].textContent = response[i].clues[0].question;
+            question_grid[12+i].textContent = response[i].clues[1].question;
+            question_grid[18+i].textContent = response[i].clues[2].question;
+            question_grid[24+i].textContent = response[i].clues[3].question;
+            question_grid[30+i].textContent = response[i].clues[4].question;
+        }
+        for(let i = 6; i < question_grid.length; i++) {
+            questions_arr.push(question_grid[i].textContent);
+        }
+        for(let i = 0; i < 6; i++) {
+            question_grid[0+i].textContent = response[i].title;
+            question_grid[6+i].textContent = response[i].clues[0].answer;
+            question_grid[12+i].textContent = response[i].clues[1].answer;
+            question_grid[18+i].textContent = response[i].clues[2].answer;
+            question_grid[24+i].textContent = response[i].clues[3].answer;
+            question_grid[30+i].textContent = response[i].clues[4].answer;
+        }
+        for(let i = 6; i < question_grid.length; i++) {
+            answers_arr.push(question_grid[i].textContent);
+        }
+        console.log(questions_arr);
+        console.log(answers_arr);
+        hide_questions();
+        //question_grid.forEach(function(block) {
+            
+        //})
     })
 }
 
+
+hide_questions();
 display_questions();
+
+// css text stroke: https://www.codesdope.com/blog/article/adding-outline-to-text-using-css/
+// hide scrollbars: https://www.w3schools.com/howto/howto_css_hide_scrollbars.asp
