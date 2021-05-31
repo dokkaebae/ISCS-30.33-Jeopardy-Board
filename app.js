0c73c28aac5171047750679dfe5cdcba3efb2808
Kirby Ezekiel <kirbyezekielsantos@gmail.com>
2021-06-01 02:51:00 +0800

    Finished pass 2, added scoreboard, questions, and a the answer input works but is not fully functional.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let questions_arr = [];
 let answers_arr = [];
+let selected;
 
 let question_grid = document.querySelectorAll(".question-block");
 let question_row = document.querySelectorAll(".question-row");
-console.log(question_grid);
+let submit_btn = document.querySelector("#answer-submit");
 
 function get_categories() {
     console.log("getting category...");
@@ -50,12 +51,18 @@ function hide_questions() {
         if (val === 0) {
             val += 200;
         } else {
-            row.childNodes[1].textContent = "$"+val;
-            row.childNodes[3].textContent = "$"+val;
-            row.childNodes[5].textContent = "$"+val;
-            row.childNodes[7].textContent = "$"+val;
-            row.childNodes[9].textContent = "$"+val;
-            row.childNodes[11].textContent = "$"+val;
+            for (let i = 1; i <= 11; i+=2) {
+                if (row.childNodes[i].classList.contains("answered")) {
+                    continue;
+                }
+                row.childNodes[i].textContent = "$"+val;
+            }
+            // row.childNodes[1].textContent = "$"+val;
+            // row.childNodes[3].textContent = "$"+val;
+            // row.childNodes[5].textContent = "$"+val;
+            // row.childNodes[7].textContent = "$"+val;
+            // row.childNodes[9].textContent = "$"+val;
+            // row.childNodes[11].textContent = "$"+val;
             val+=200;
         }
     })
@@ -98,15 +105,41 @@ function display_questions() {
         console.log(questions_arr);
         console.log(answers_arr);
         hide_questions();
-        //question_grid.forEach(function(block) {
-            
-        //})
     })
 }
 
-
-hide_questions();
+// the setup
 display_questions();
 
+
+// user interaction
+function select_question() {
+    question_grid.forEach(function(block) {
+        block.addEventListener("click", function() {
+            if (this.classList.contains("hidden")) {
+                hide_questions();
+                this.textContent = questions_arr[this.id]
+                this.classList.replace("hidden", "question");
+                selected = this.id;
+            }
+        })
+        
+    })
+}
+
+submit_btn.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(selected==null) {
+        alert("Please select a question first.")
+    } else {
+        var ans = document.querySelector("#answer");
+        if (ans.value.toUpperCase() == answers_arr[selected].toUpperCase()) {
+            console.log("correct");
+        }
+    }
+})
+
+select_question();
 // css text stroke: https://www.codesdope.com/blog/article/adding-outline-to-text-using-css/
 // hide scrollbars: https://www.w3schools.com/howto/howto_css_hide_scrollbars.asp
+// prevent refresh after submit: https://devnet.kentico.com/questions/prevent-page-refreshing-when-clicking-a-button
